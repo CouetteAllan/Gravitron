036b8fce4612a8572b901340f81ed1200886e141
oct0punk <elric.emile@gmail.com>
2020-04-23 10:11:29 +0200

    JMT

---
+++ Assets/Scripts/JeanMichelTesteur.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeanMichelTesteur : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Move();
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 move = new Vector2(horizontal, 0);
+        Vector2 position = this.rb2d.position;
+        position += move * this.speed * Time.deltaTime;
+        this.rb2d.MovePosition(position);
+    }
+}

c4f5f1f887b8bf94b5de5b7f0efa69fd47a26e3a
SkyLaugh987 <skylaughblue@gmail.com>
2020-04-23 15:33:21 +0200

    finite state machine

---
+++ Assets/Scripts/GravityBaseState.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+public abstract class GravityBaseState : MonoBehaviour
+{
+    public abstract void EnterState(GravityController gravity);
+
+    public abstract void Update(GravityController gravity);
+
+    public abstract void OnCollisionEnter2D(GravityController gravity);
+}
---
+++ Assets/Scripts/GravityLeftState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityLeftState : GravityBaseState
+{
+    public override void EnterState(GravityController gravity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnCollisionEnter2D(GravityController gravity)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(GravityController gravity)
+    {
+        throw new System.NotImplementedException();
+    }
+}

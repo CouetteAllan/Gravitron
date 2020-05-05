74ab028c3c1d6cdcc908229cfe086ca71c8a972e
SkyLaugh987 <skylaughblue@gmail.com>
2020-05-05 17:49:12 +0200

    porte movible et button relation

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private bool active = false;
+    public bool Active
+    {
+        get { return active; }
+        set { active = value; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        JeanMichelTesteur jean = collision.GetComponent<JeanMichelTesteur>();
+        if (jean == null && active == true)
+        {
+            active = false;
+        }
+        if (jean == null)
+        {
+            active = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Porte.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porte : MonoBehaviour
+{
+    Button button = new Button();
+    float moveDistance = 5f;
+    private Vector2 targetMovePosition;
+    Vector3 movDirHor = new Vector3(1, 0, 0);
+    Vector3 movDirVer;
+
+    private 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MoveHorizontal()
+    {
+        if (button.Active == true)
+        {
+            
+            this.targetMovePosition = transform.position + (moveDistance * movDirHor);
+            this.transform.position = Vector2.MoveTowards(this.transform.position, this.targetMovePosition, 50.0f * Time.deltaTime);
+            if (Vector3.Distance(this.transform.position, this.targetMovePosition) < 0.05f)
+            {
+                this.transform.position = this.targetMovePosition;
+            }
+        }
+    }
+}

86bb9d59b9762b18b4293e948a1d0eec9cb659f4
oct0punk <elric.emile@gmail.com>
2020-05-07 10:12:16 +0200

    Collectible d'énergie

--- Assets/Scripts/UIEnergyScript.cs
+++ Assets/Scripts/UIEnergyScript.cs
@@ -5,33 +5,47 @@ using UnityEngine.UI;
 
 public class UIEnergyScript : MonoBehaviour
 {
+    public static UIEnergyScript Instance { get; private set; } 
     [SerializeField] private Image energyBar;
     [SerializeField] private Text energyTxt;
     private int energy = 0;
+    private int half = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void Start()
     {
         ChangeEnergy(0);
     }
 
-    private void Update()
+    public void ChangeEnergy(int amount, bool semi = false)
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (semi)
         {
-            ChangeEnergy(-1);
+            half++;
+            if (half >= 2)
+            {
+                half -= 2;
+                energy++;
+            }
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        else
         {
-            ChangeEnergy(1);
-        }
-    }
-
-    public void ChangeEnergy(int amount)
-    {
-        energy += amount;
-        if (energy < 0)
-        {
-            energy = 0;
+            energy += amount;
+            if (energy < 0)
+            {
+                energy = 0;
+            }
         }
         energyTxt.text = " X" + energy;
     }

9bb251539147206f7bb57846293f8d13bdc95028
Allan <couetteallan@gmail.com>
2020-05-12 09:33:04 +0200

    on peut choisir la gravivté au début du niveau dans le manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Vector2 localGravity;
-    [SerializeField]
-    private string gravityInputDirection = "down";
+    
 
     private Vector2 direction;
 
@@ -34,6 +33,7 @@ public class GameManager : MonoBehaviour
         MainMenu,
     }
 
+    [SerializeField]
     private Gravity gravity;
     public enum Gravity
     {

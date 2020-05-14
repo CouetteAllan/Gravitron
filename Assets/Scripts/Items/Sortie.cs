03ddb79d3600493f385a6e14a273d3e8a6cb9716
SkyLaugh987 <skylaughblue@gmail.com>
2020-05-14 10:49:50 +0200

    timer

--- Assets/Scripts/Items/Sortie.cs
+++ Assets/Scripts/Items/Sortie.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Sortie : MonoBehaviour
 {
+    [SerializeField]
+    private Timer t;      
     private void OnTriggerEnter2D(Collider2D collision)
     {
         JeanMichelTesteur player = collision.GetComponent <JeanMichelTesteur>();
         if (player != null)
         {
+            t.Finish();
             GameManager.Instance.ChangeState(GameManager.GameState.Victory);
         }
     }

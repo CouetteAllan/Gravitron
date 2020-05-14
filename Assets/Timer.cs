b66c975f7190b0c13e3cdc5e365472f0f2226141
SkyLaugh987 <skylaughblue@gmail.com>
2020-05-14 10:16:40 +0200

    animator (allan)

---
+++ Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerText;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeSinceStart = Time.time - startTime;
+
+        string minutes = ((int)timeSinceStart / 60).ToString();
+        string seconds = (timeSinceStart % 60).ToString();
+    }
+}

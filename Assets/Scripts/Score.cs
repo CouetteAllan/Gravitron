8f61d953c5b5ff10633884f94563771cfc68159a
SkyLaugh987 <skylaughblue@gmail.com>
2020-05-19 14:33:39 +0200

    étoiles/badge

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    CarteMere carte;
+    [SerializeField] private UIManager bond;
+    [SerializeField] private Text score;
+
+    int count = 0;
+
+    public void StarDisplay()
+    {
+        
+    }
+}

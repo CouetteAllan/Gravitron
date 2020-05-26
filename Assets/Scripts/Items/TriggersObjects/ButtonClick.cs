c224abc9055f1a88d343844f86a3a824e0065307
Allan <couetteallan@gmail.com>
2020-05-26 18:36:43 +0200

    bite

--- Assets/Scripts/Items/TriggersObjects/ButtonClick.cs
+++ Assets/Scripts/Items/TriggersObjects/ButtonClick.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonClick : TriggerObjects
-{ 
+{
     /*private int directionVectorPorte = 1;
     public int DirectionVectorPorte
     {
         get { return directionVectorPorte; }
         private set { return; }
     }*/
-   
+    [SerializeField]
+    private Sprite pressed;
+    [SerializeField]
+    private Sprite nopressed;
 
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -24,6 +27,15 @@ public class ButtonClick : TriggerObjects
                 Debug.Log("Bouton préssé, " + Active);
                 //directionVectorPorte = - directionVectorPorte;
                 Snap();
+
+                if (active == true)
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = pressed;
+                }
+                else if (active == false)
+                {
+                    this.GetComponent<SpriteRenderer>().sprite = nopressed;
+                }
             }
         }
     }

513cfdc327a223e13d4e81183bb7d49aea285622
SkyLaugh987 <skylaughblue@gmail.com>
2020-05-26 18:11:45 +0200

    zap de merde!

--- Assets/Scripts/Items/Zap.cs
+++ Assets/Scripts/Items/Zap.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Zap : InteractableObjects
 {
-    [SerializeField]
-    private CamShake shake;
-    [SerializeField]
-    private UIManager t;
 
 
 
@@ -24,11 +20,10 @@ public class Zap : InteractableObjects
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        this.shake.Shake();
+
         JeanMichelTesteur jean = collision.GetComponent <JeanMichelTesteur>();
         if (jean != null)
         {
-            t.TimerAtTheEnd();
             jean.Dead(0.2f);
         }
     }

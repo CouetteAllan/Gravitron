81f3761895486a0da23d452e73189206566918b3
oct0punk <elric.emile@gmail.com>
2020-05-26 11:07:28 +0200

    4 Prefab de Levier
    4 Orientations

--- Assets/Scripts/Items/Levier.cs
+++ Assets/Scripts/Items/Levier.cs
@@ -4,26 +4,117 @@ using UnityEngine;
 
 public class Levier : MonoBehaviour
 {
-    [SerializeField] private Transform parent;
+
     [SerializeField] private float rotationSpeed = 1;
 
-    private Quaternion trainedRotation = Quaternion.Euler(0, 0, 45);
-    private Quaternion bentRotation = Quaternion.Euler(0, 0, 135);
+    private Quaternion trainedRotation;
+    private Quaternion bentRotation;
 
-    private bool trained = true;
+    [SerializeField] private bool trained = false;
+    private Orientation lastTrain;
+
+
+    [SerializeField] private Orientation sens;
+    private enum Orientation
+    {
+        up,
+        right,
+        down,
+        left,
+    }
+
+    private void Start()
+    {
+        Levier[] leviers = FindObjectsOfType<Levier>();
+        foreach (Levier l in leviers)
+        {
+            Debug.Log(l + " est dans le sens " + l.sens);
+        }
+        
+        switch (sens)
+        {
+            case Orientation.right:
+                trainedRotation = Quaternion.Euler(0, 0, 45);
+                bentRotation = Quaternion.Euler(0, 0, 135);
+                break;
+            case Orientation.left:
+                trainedRotation = Quaternion.Euler(0, 0, -45);
+                bentRotation = Quaternion.Euler(0, 0, -135);
+                break;
+            case Orientation.down:
+                trainedRotation = Quaternion.Euler(0, 0, 45);
+                bentRotation = Quaternion.Euler(0, 0, -45);
+                break;
+            case Orientation.up:
+                trainedRotation = Quaternion.Euler(0, 0, 135);
+                bentRotation = Quaternion.Euler(0, 0, -135);
+                break;
+        }
+    }
+
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
             trained = !trained;
-            InvokeRepeating("Train", 0, Time.deltaTime);
+            CancelInvoke("ActivateLever");
+            InvokeRepeating("ActivateLever", 0, Time.deltaTime);
+            StopAllCoroutines();
+            StartCoroutine(StopTrain());
         }
     }
 
 
+    public void Train(GameManager.Gravity gravity)
+    {
+        switch (gravity)
+        {
+            case GameManager.Gravity.Down:
+                if (lastTrain != Orientation.down)
+                {
+                    if (sens == Orientation.right || sens == Orientation.left)
+                    {
+                        lastTrain = Orientation.down;
+                        ActivateLever();
+                    }
+                }
+                break;
+            case GameManager.Gravity.Up:
+                if (lastTrain != Orientation.up)
+                {
+                    if (sens == Orientation.right || sens == Orientation.left)
+                    {
+                        lastTrain = Orientation.up;
+                        ActivateLever();
+                    }
+                }
+                break;
+            case GameManager.Gravity.Left:
+                if (lastTrain != Orientation.left)
+                {
+                    if (sens == Orientation.up || sens == Orientation.down)
+                    {
+                        lastTrain = Orientation.left;
+                        ActivateLever();
+                    }
+                }
+                break;
+            case GameManager.Gravity.Right:
+                if (lastTrain != Orientation.right)
+                {
+                    if (sens == Orientation.up || sens == Orientation.down)
+                    {
+                        lastTrain = Orientation.right;
+                        ActivateLever();
+                    }
+                }
+                break;
+        }
+    }
 
-    public void Train()
+
+    public void ActivateLever()
     {
         Vector3 move = transform.position;
 
@@ -34,11 +125,15 @@ public class Levier : MonoBehaviour
 
 
         transform.localPosition = new Vector3(-transform.localRotation.z * 1.5f, 0, 0);
-
-        if (transform.rotation == Quaternion.Euler(0,0,50) || transform.rotation == Quaternion.Euler(0, 0, 130))
-        {
-            Debug.Log("NotInvokating");
-            CancelInvoke();
-        }
     }
+
+
+    IEnumerator StopTrain()
+    {
+        yield return new WaitForSeconds(1);
+        CancelInvoke();
+        Debug.Log("InInvokating");
+    }
+
+
 }

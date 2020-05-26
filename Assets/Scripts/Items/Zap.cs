75fee42614e0ec087f9d97f618ab25f282d2a8dd
Allan <couetteallan@gmail.com>
2020-05-26 18:05:44 +0200

    quoi

---
+++ Assets/Scripts/Items/Zap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zap : InteractableObjects
+{
+    [SerializeField]
+    private CamShake shake;
+    [SerializeField]
+    private UIManager t;
+
+
+
+    protected override void ActivateObject()
+    {
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    protected override void DisabledObject()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        this.shake.Shake();
+        JeanMichelTesteur jean = collision.GetComponent <JeanMichelTesteur>();
+        if (jean != null)
+        {
+            t.TimerAtTheEnd();
+            jean.Dead(0.2f);
+        }
+    }
+}

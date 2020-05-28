380e86f13a0a2d43d125c885e9c6b8b12e274f0f
oct0punk <elric.emile@gmail.com>
2020-05-28 16:56:12 +0200

    prefab carte mère

--- Assets/Scripts/Items/CarteMere.cs
+++ Assets/Scripts/Items/CarteMere.cs
@@ -5,9 +5,22 @@ using UnityEngine.UI;
 
 public class CarteMere : MonoBehaviour
 {
+    [SerializeField] private GameObject halo;
+    public float aura;
+    public float amplitude;
+
+
     [SerializeField]
     private Text cacahuete;
     public bool collected = false;
+
+    private void Update()
+    {
+        float haloScale = aura + Mathf.Sin(Time.timeSinceLevelLoad) * amplitude;
+        halo.transform.localScale = new Vector3(haloScale, haloScale, 0);
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         JeanMichelTesteur jean = collision.GetComponent<JeanMichelTesteur>();

05a76fc291396f46b8c11581a86a2bb590c3cb44
oct0punk <elric.emile@gmail.com>
2020-06-02 09:57:21 +0200

    oui

--- Assets/Scripts/Items/Sortie.cs
+++ Assets/Scripts/Items/Sortie.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Sortie : MonoBehaviour
 {
-    
 
+    [SerializeField] private int levelIndex = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         JeanMichelTesteur player = collision.GetComponent <JeanMichelTesteur>();
         if (player != null)
         {
-            LevelList.lastLevelUnlock++;
+            if (levelIndex == LevelList.lastLevelUnlock)
+            {
+                LevelList.lastLevelUnlock++;
+            }
             GameManager.Instance.ChangeState(GameManager.GameState.Victory);
         }
         

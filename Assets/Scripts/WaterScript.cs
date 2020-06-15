c1a61feed2cb122cce9fa80889a7edb5d40f8737
oct0punk <elric.emile@gmail.com>
2020-06-16 01:04:44 +0200

    feedBack WaterbackGround
    le niveau d'eau baisse en fonction de l'énergie qu'il reste au joueur

---
+++ Assets/Scripts/WaterScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+    public static WaterScript Instance { get; private set; }
+
+    [SerializeField] private List<GameObject> waterParticles = new List<GameObject>();
+
+    private void Awake()
+    {
+        Instance = Instance == null ? this : null;
+    }
+
+    private void Start()
+    {
+        ChangeWater(UIManager.Instance.SendEnergy());
+    }
+
+    public void ChangeWater(int currentEnergy)
+    {
+        Debug.Log("WaterProcessing");
+        Debug.Log("currentEnergy = " + currentEnergy);
+
+        for (int i = 0; i < waterParticles.Count; i++ )
+        {
+            if (5 * currentEnergy > i)
+            {
+                Debug.Log("2 x currentEnergy < " + i);
+                Debug.Log("Water.SetActive(true)");
+                waterParticles[i].SetActive(true);
+            }
+            else
+            {
+                Debug.Log("2 x currentEnergy > " + i);
+                Debug.Log("water.SetActive(false)");
+                waterParticles[i].SetActive(false);
+            }
+        }
+    }
+}
